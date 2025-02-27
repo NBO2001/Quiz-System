c8c28848ed8ea1f382559915329b27c4d2aaef6a
nbo2001 <natanael.oliveira@jusbrasil.com.br>
2025-02-27 19:58:11 -0400

    fix: code style

--- backend/app/controllers/QuizController.scala
+++ backend/app/controllers/QuizController.scala
@@ -10,18 +10,17 @@ import com.nbo.repositories.QuestionRepository
 import scala.concurrent.{ExecutionContext, Future}
 import play.api.Logging
 
+@Singleton
 class QuizController @Inject()(val controllerComponents: ControllerComponents, questionRepository: QuestionRepository)(implicit val ec: ExecutionContext)
   extends BaseController with Logging {
   
   def getAll(): Action[AnyContent] = Action.async {
     questionRepository.getAllQuestions().map { questions =>
-        Ok(Json.toJson(questions))
+      Ok(Json.toJson(questions))
     }
-
   }
 
   def getById(question_id: String, answered: Boolean = true): Action[AnyContent] = Action.async {
-    
     questionRepository.getQuestion(question_id).map {
       case Some(q) =>
         if (answered) {
@@ -34,37 +33,27 @@ class QuizController @Inject()(val controllerComponents: ControllerComponents, q
   }
 
   def checkAnswer(question_id: String): Action[JsValue] = Action.async(parse.json) { request =>
-
     request.body.validate[OptionDto] match {
       case JsSuccess(answer, _) =>
         questionRepository.checkAnswer(question_id, answer.text).map { isCorrect =>
           Ok(Json.obj("isCorrect" -> isCorrect))
         }
       case JsError(errors) =>
-        Future.successful(BadRequest(Json.obj("error" -> "Invalid JSON", "details" -> errors.mkString(", ")))
-      )
+        Future.successful(BadRequest(Json.obj("error" -> "Invalid JSON", "details" -> errors.mkString(", "))))
     }
   }
 
-  
-  def create(): Action[JsValue] = Action.async(parse.json) { request => {
-    
-      request.body.validate[QuestionDto] match {
-      
-        case JsSuccess(newQuestion, _) => 
-       
-          val question = QuestionDto(newQuestion.content, newQuestion.options)
-          
-
-          questionRepository.insertQuestion(question).map { question =>
-            Created(Json.toJson(question))
-          }
-        case JsError(errors) =>
-          Future.successful(BadRequest(Json.obj("error" -> "Invalid JSON", "details" -> errors.mkString(", "))))
-      }
+  def create(): Action[JsValue] = Action.async(parse.json) { request =>
+    request.body.validate[QuestionDto] match {
+      case JsSuccess(newQuestion, _) =>
+        val question = QuestionDto(newQuestion.content, newQuestion.options)
+        questionRepository.insertQuestion(question).map { question =>
+          Created(Json.toJson(question))
+        }
+      case JsError(errors) =>
+        Future.successful(BadRequest(Json.obj("error" -> "Invalid JSON", "details" -> errors.mkString(", "))))
     }
   }
-
 
   def delete(question_id: String): Action[AnyContent] = Action.async {
     questionRepository.deleteQuestion(question_id).map { _ =>
